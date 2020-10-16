6932b4619661221b604e09cb443ea506b2ff42e6
ulvi_199517 <ulvi199517@gmail.com>
2020-10-16 01:51:58 -0400

    adding styling to the page

--- src/App.js
+++ src/App.js
@@ -25,10 +25,8 @@ class App extends Component {
       );
     return (
       <div className="App">
-        <SearchBox
-          placeholder="search monsters"
-          handleChange={this.handleChange}
-        />
+        <h1> Monsters Rolodex </h1>
+        <SearchBox placeholder="search monsters" handleChange={this.handleChange}/>
         <CardList monsters={filteredMonsters}/>
       </div>
     );
